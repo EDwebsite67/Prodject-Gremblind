6f81d7911b7f96afa60909e9ccd246efc65bd98a
7h30-byte <7h30.maker@gmail.com>
2026-09-10 14:48:39 -0600

    more

--- scripts/Gremblind.gd
+++ scripts/Gremblind.gd
@@ -19,10 +19,24 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		$Gremblind/AnimationPlayer.play("ArmatureAction")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		$Gremblind/AnimationPlayer.stop()
+	
+	if Input.is_action_pressed("left"):
+		rotation.y = 270
+	
+	if Input.is_action_pressed("right"):
+		rotation.y = 90
+	
+	if Input.is_action_pressed("up"):
+		rotation.y = 0
+	
+	if Input.is_action_pressed("down"):
+		rotation.y = 180
 
 	move_and_slide()
